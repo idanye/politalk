2ca58bafc4d0576beb37c4b3999680d4b2885617
idanye <idanyehiel@gmail.com>
2023-11-28 18:16:46 +0200

    Continued working on getting contact- primary with Google api
    Created: get_new_connections, extract_search_text_from_url, get_search_text_url.

--- scraper.py
+++ scraper.py
@@ -3,62 +3,116 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from seleniumwire import webdriver as wiredriver
+from urllib.parse import parse_qs, urlparse
 import json
 import functions
 import requests
 import time
 
-def get_new_connections(school_name):
-    """ Returns a list of ?new? linkedin links from the recruitin website from a chosen university
+def get_search_text_url(school_name):
+    """ Returns a str of the search text url from the recruitin website from a chosen university
     """
     URL = "https://recruitin.net/"
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
     options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-sync')
+    options.add_argument('--headless')
+    options.add_argument('--ignore-certificate-errors')
+    options.set_preference('devtools.console.stdout.content', True)
 
-    driver = webdriver.Chrome(options=options)
+    # Set up Selenium WebDriver with proxy
+    driver = webdriver.Firefox(options=options)
     driver.get(URL)
-    linkedin_links = []
 
     try:
         include_keywords_element = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "input#include-keywords.field__control.field__control--input")))
-        
         include_keywords_element.clear()
 
-        # Send keys to the input field
         include_keywords_element.send_keys("Student at " + school_name)
+
+        print(f"searched student at: {school_name}")
 
         find_button = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "button#fly-baby.btn.btn--block.btn--primary")))
         find_button.click()
 
-        find_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "a#resultButton.btn.btn--block.btn--primary")))
-        find_button.click()
+        print(f"clicked button 1")
 
-        WebDriverWait(driver, 20).until(
-            EC.url_contains("google.com"))
+        input_element = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input#fe_text.field__control.field__control--lg")))
 
-        linkedin_links.append(-1)
+        input_value_url = input_element.get_attribute("value")
 
-        # Find all anchor tags with a 'href' attribute containing "linkedin.com"
-        linkedin_links_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'a[jsname="UWckNb"][href*="linkedin.com"]')))
-
-        linkedin_links.append(-2)
-        # Print the links
-        for link in linkedin_links_element:
-            linkedin_links.append(link.get_attribute("href"))
+    except TimeoutException as te:
+        print(f"Timeout error occurred: {te}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
     finally:
         driver.quit()
-        print(linkedin_links)
+        print(input_value_url)
 
-get_new_connections(school_name="Harvard")
+
+def extract_search_text_from_url(url):
+    """Recieves a url and returns the string search text from it
+    """
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+
+    # Extract the 'q' parameter, which contains the search text
+    search_text = query_params.get('q', [''])[0]
+
+    return search_text
+
+
+def get_new_connections(search_text, output_file="linkedin_urls_trialtwo.json"):
+    """Recieves the search text and returns a set of the linkedin urls from the search and saves it as a text file
+    """
+    headers = {"apikey": "70eeee00-8dfa-11ee-b02d-f36f343cfa0b"}
+
+    params = (
+    ("q", search_text),
+    ("num","100"),
+    ("start","2"),
+    );
+
+    response = requests.get('https://app.zenserp.com/api/v2/search', headers=headers, params=params);
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON content
+        url_links = set()
+
+        json_content = response.json()
+        pos_responses = json_content.get("organic", [])
+        
+        # Extract URLs from the "organic" results
+        for position in pos_responses:
+            url_links.add(position.get("url", ""))
+
+        # Save the results to a JSON file
+        with open(output_file, "w") as file:
+            json.dump(list(url_links), file)
+        
+        return list(url_links)
+
+    else:
+        print(f"Error: {response.status_code}")
+
+
+# Example URL
+# url = "https://www.google.com/search?q=+%22Student+at+Harvard%22%20OR%20%22Student+at+Stanford%22%20OR%20%22Student+at+Princeton%22%20OR%20%22Student+at+Colombia%22%20-intitle:%22profiles%22%20-inurl:%22dir/+%22+site:linkedin.com/in/+OR+site:linkedin.com/pub/"
+
+# search_text = extract_search_text_from_url(url)
+# result_list = get_new_connections(search_text)
+
+# print(result_list)
+
+get_search_text(school_name="Harvard")
 
 
 # # Authenticate using any Linkedin account credentials
@@ -76,47 +130,4 @@ get_new_connections(school_name="Harvard")
 
 
 # print(json.dumps(profile, indent=2))
- 
-
-
-
- #From Nimrod
-# CURRENT_PATH = _file_.split("xbot.py")[0] #change string to the file name
-
-# self.options.add_argument("--user-data-dir=" + CURRENT_PATH + "cookies")
-
-# def login(self, headless: bool = False) -> bool:
-#         max_tries = 3
-#         current_try = 0
-#         login_successful = False
-
-#         if headless:
-#             self.options.add_argument('--headless=new')
-
-#         self.start() #Function that creates the driver == connection
-
-#         # fetches the login page
-#         self.open_page('https://twitter.com/')
-
-#         while not login_successful and current_try <= max_tries:
-#             login_successful = self.is_logged_in()
-#             current_try += 1
-#             # let cookies update
-#             time.sleep(2)
-
-#         self.quit() # driver.close()
-#         return login_successful # Check if you were able to connect
-
-# def is_logged_in(self) -> bool:
-#     xpath = "//a[@data-testid='SideNav_NewTweet_Button']" # catches the connection object
-
-#     try:
-#         WebDriverWait(self.bot, 30).until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
-#         validation = True
-#     except Exception as e:
-#         logging.error("Exception occurred", exc_info=True)
-#         validation = False
-
-#     return validation
-
 
