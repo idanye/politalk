41475dd7534d5ec9b71bdf9744c4eefefb4f5833
Ran Zaaroor <Ran.zaaroor@gmail.com>
2023-11-28 19:19:21 +0200

    Added PyQt5 GUI and LinkedIn login options

--- GUI/UserSession.py
+++ GUI/UserSession.py
@@ -1,14 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import time
+import logging
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as ec
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+#options.add_argument("--headless=new")
 
 class UserSession:
     def __init__(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = options)
+        try:
+            self.driver = webdriver.Chrome(options = options)
+        except:
+            self.driver = webdriver.Chrome(ChromeDriverManager().install(), options = options)
         self.cookies = None
+
+    def start(self):
+        self.driver.get("https://www.linkedin.com/login")
+        self.driver.implicitly_wait(1)
+
+    def quit(self):
+        self.driver.quit()
 
     def save_cookies(self):
         self.cookies = self.driver.get_cookies()
@@ -25,3 +41,37 @@ class UserSession:
         self.driver.maximize_window()
 
     # Add other necessary methods like login, logout, etc.
+
+    def login(self, headless: bool = False) -> bool:
+        max_tries = 3
+        current_try = 0
+        login_successful = False
+
+        if headless:
+            self.options.add_argument('--headless=new')
+
+        self.start()
+
+        # fetches the login page
+        self.open_page('https://www.linkedin.com/login')
+
+        while not login_successful and current_try <= max_tries:
+            login_successful = self.is_logged_in()
+            current_try += 1
+            # let cookies update
+            time.sleep(2)
+
+        self.quit()
+        return login_successful
+
+    def is_logged_in(self) -> bool:
+        xpath = "//a[@data-testid='SideNav_NewTweet_Button']"
+
+        try:
+            WebDriverWait(self.bot, 30).until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
+            validation = True
+        except Exception as e:
+            logging.error("Exception occurred", exc_info=True)
+            validation = False
+
+        return validation
---
+++ GUI/politalkGUI.py
@@ -0,0 +1,171 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from UserSession import UserSession
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(596, 438)
+        MainWindow.setStyleSheet("background-color: rgb(231, 231, 231);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.LoginButton = QtWidgets.QPushButton(self.centralwidget)
+        self.LoginButton.setGeometry(QtCore.QRect(70, 210, 171, 61))
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.PlaceholderText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.PlaceholderText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(72, 105, 137))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(231, 231, 231, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.PlaceholderText, brush)
+        self.LoginButton.setPalette(palette)
+        font = QtGui.QFont()
+        font.setFamily("Arial Black")
+        font.setPointSize(13)
+        font.setBold(True)
+        font.setWeight(75)
+        self.LoginButton.setFont(font)
+        self.LoginButton.setAutoFillBackground(False)
+        self.LoginButton.setStyleSheet("QPushButton {\n"
+"    background-color: rgb(72, 105, 137);\n"
+"    border: none;\n"
+"    color: rgb(231, 231, 231);\n"
+"    border-radius: 10px;\n"
+"    border-left: 2px solid rgb(55, 81, 108);\n"
+"    border-right: 2px solid rgb(55, 81, 108);\n"
+"    border-bottom: 5px solid rgb(55, 81, 108);\n"
+"    padding-top: 5px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(80, 116, 155);\n"
+"    border-left: 2px solid rgb(55, 81, 108);\n"
+"    border-right: 2px solid rgb(55, 81, 108);\n"
+"    border-bottom: 5px solid rgb(55, 81, 108);\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: rgb(40, 58, 77);\n"
+"    border-left: 2px solid rgb(55, 81, 108);\n"
+"    border-right: 2px solid rgb(55, 81, 108);\n"
+"    border-bottom: 5px solid rgb(55, 81, 108);\n"
+"    padding-top: -5px;\n"
+"    border-bottom: none;\n"
+"}\n"
+"")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("linkedin-logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.LoginButton.setIcon(icon)
+        self.LoginButton.setObjectName("LoginButton")
+        self.Logo = QtWidgets.QLabel(self.centralwidget)
+        self.Logo.setGeometry(QtCore.QRect(40, 10, 231, 181))
+        self.Logo.setText("")
+        self.Logo.setPixmap(QtGui.QPixmap("talk.png"))
+        self.Logo.setScaledContents(True)
+        self.Logo.setObjectName("Logo")
+        self.Table = QtWidgets.QTableView(self.centralwidget)
+        self.Table.setGeometry(QtCore.QRect(310, 10, 261, 401))
+        self.Table.setStyleSheet("QTableView{\n"
+"    background-color: rgb(72, 105, 137);\n"
+"    border: none;\n"
+"    color: rgb(231, 231, 231);\n"
+"    border-radius: 3px;\n"
+"    border-top: 4px solid rgb(55, 81, 108);\n"
+"    border-left: 4px solid rgb(55, 81, 108);\n"
+"    border-right: 4px solid rgb(55, 81, 108);\n"
+"    border-bottom: 4px solid rgb(55, 81, 108);\n"
+"}")
+        self.Table.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Table.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Table.setMidLineWidth(1)
+        self.Table.setGridStyle(QtCore.Qt.DashLine)
+        self.Table.setSortingEnabled(False)
+        self.Table.setObjectName("Table")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 596, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        #On click event for login button
+        self.LoginButton.clicked.connect(self.login_clicked)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.LoginButton.setText(_translate("MainWindow", "Log into LinkedIn"))
+
+    def login_clicked(self) -> None:
+        UserSession().start()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
