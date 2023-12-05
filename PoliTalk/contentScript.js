67905fcdc2614c6c4f1fdc1791756629f1b17441
Ran Zaaroor <Ran.zaaroor@gmail.com>
2023-12-05 18:33:24 +0200

    Added popup and profile changing functionality to the extension button

--- PoliTalk/contentScript.js
+++ PoliTalk/contentScript.js
@@ -1,9 +1,17 @@
-function injectCSS() {
+function injectCSS(callback) {
   const link = document.createElement('link');
   link.href = chrome.runtime.getURL('extensionButton.css');
   link.type = 'text/css';
   link.rel = 'stylesheet';
+  link.onload = callback;
   document.head.appendChild(link);
+}
+
+function makeButtonVisible() {
+  const extensionButton = document.querySelector('.extension_button');
+  if (extensionButton) {
+    extensionButton.style.visibility = 'visible';
+  }
 }
 
 function addHoverEffect(button, icon) {
@@ -26,6 +34,7 @@ function createLinkedInButton() {
   if (navItems) {
     const extensionButton = document.createElement('li');
     extensionButton.className = 'extension_button';
+    extensionButton.style.visibility = 'hidden';
 
     // Create a container for the icon and text
     const iconContainer = document.createElement('div');
@@ -41,16 +50,143 @@ function createLinkedInButton() {
     text.textContent = 'PoliTalk';
     iconContainer.appendChild(text);
 
+    // Create and add the line
+    const line = document.createElement('div');
+    line.className = 'underline';
+    iconContainer.appendChild(line);
+
     // Add the icon container to the button
     extensionButton.appendChild(iconContainer);
 
     // Style the button and icon
     addHoverEffect(extensionButton, icon);
 
-    extensionButton.addEventListener('click', handleButtonClick);
+    // Create the popup
+    createPopup();
+
+    extensionButton.addEventListener('click', function() {
+      this.classList.toggle('show-line');
+      const popup = document.getElementById('politalk_popup');
+      if (popup) {
+        if (!popup.classList.contains('show-popup')) {
+          const rect = this.getBoundingClientRect();
+          popup.style.top = `${rect.bottom}px`; // Position below the button
+          popup.style.left = `${rect.left}px`; // Align with the left edge of the button
+        }
+        popup.classList.toggle('show-popup');
+      }
+    });
     navItems.insertBefore(extensionButton, navItems.firstChild);
   }
 }
+
+function createPopup() {
+  // Check if the popup already exists to avoid duplicates
+  if (document.getElementById('politalk_popup')) {
+    return;
+  }
+
+  // Create the popup container
+  const popup = document.createElement('div');
+  popup.id = 'politalk_popup';
+  popup.className = 'politalk_popup';
+
+  // Create and add the title
+  const title = document.createElement('h2');
+  title.textContent = 'Start PoliTalking With:';
+  popup.appendChild(title);
+
+  // Example data for the cards
+  const exampleProfiles = [
+    {
+      name: 'Lia Opperman',
+      image: 'https://media.licdn.com/dms/image/D4E03AQGWrT4EHIcWeA/profile-displayphoto-shrink_100_100/0/1700504577540?e=1706745600&v=beta&t=bLXgZZ4b_taqHccpD-SHR-mrQ2UvnxclmhJM1pV-ExY', // Replace with actual image path or URL
+      description: 'Policy and Journalism Student at Princeton University'
+    },
+    {
+      name: 'Christina Sorochinsky',
+      image: 'https://media.licdn.com/dms/image/C4D03AQFVq0B02xqunA/profile-displayphoto-shrink_100_100/0/1662696016084?e=1706745600&v=beta&t=KBrSAM4aJkUCGUrCglpPERvYp2YahLzCAe9qwgWxNSA',
+      description: 'Student at Harvard University'
+    },
+    {
+      name: 'Lily Zamora',
+      image: 'https://media.licdn.com/dms/image/C4E03AQFT3SJpwRxBDA/profile-displayphoto-shrink_100_100/0/1660240894014?e=1707350400&v=beta&t=cE7YSEZFLMjSqILIJSk7ZDM1wQgyuiU0LNVcMVQIBAY',
+      description: 'Student at Brown University'
+    }
+  ];
+  let currentIndex = 0;
+
+  // Update the card with the profile data
+  let lastDirection = 'right';
+  const updateProfileCard = () => {
+    if (popup.querySelector('.card')) {
+      popup.removeChild(popup.querySelector('.card'));
+    }
+
+    const profile = exampleProfiles[currentIndex];
+
+    const card = document.createElement('div');
+    card.className = 'card';
+  
+    const img = document.createElement('img');
+    img.src = profile.image;
+    img.alt = profile.name;
+    img.className = 'profile-image';
+    card.appendChild(img);
+  
+    const name = document.createElement('h3');
+    name.textContent = profile.name;
+    card.appendChild(name);
+  
+    const description = document.createElement('p');
+    description.textContent = profile.description;
+    card.appendChild(description);
+
+    // Set the animation based on the last direction
+    card.style.animation = lastDirection === 'right' 
+    ? 'slideInFromRight 0.5s ease' 
+    : 'slideInFromLeft 0.5s ease';
+    popup.appendChild(card);
+  };
+  updateProfileCard(); // Initial profile card
+
+  // Create and add navigation arrows
+  const prevArrow = document.createElement('img');
+  prevArrow.src = chrome.runtime.getURL('images/prevArrow.svg');
+  prevArrow.className = 'arrow left';
+  prevArrow.onclick = () => {
+    lastDirection = 'left';
+    currentIndex = (currentIndex - 1 + exampleProfiles.length) % exampleProfiles.length;
+    updateProfileCard();
+  };
+
+  const nextArrow = document.createElement('img');
+  nextArrow.src = chrome.runtime.getURL('images/nextArrow.svg');
+  nextArrow.className = 'arrow right';
+  nextArrow.onclick = () => {
+    lastDirection = 'right';
+    currentIndex = (currentIndex + 1) % exampleProfiles.length;
+    updateProfileCard();
+  };
+
+  popup.appendChild(prevArrow);
+  popup.appendChild(nextArrow);
+  // Append the popup to the document body
+  document.body.appendChild(popup);
+
+  popup.addEventListener('click', function(event) {
+  event.stopPropagation(); // Prevent clicks within the popup from closing it
+  });
+}
+
+document.addEventListener('click', function(event) { // 'event' is now a parameter of the function
+  const popup = document.getElementById('politalk_popup');
+  const extensionButton = document.querySelector('.extension_button');
+  if (popup && !popup.contains(event.target) && !event.target.closest('.extension_button')) {
+    popup.classList.remove('show-popup');
+    extensionButton.classList.remove('show-line');
+  }
+});
 
 const observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
@@ -62,9 +198,5 @@ const observer = new MutationObserver(function (mutations) {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
-injectCSS();
+injectCSS(makeButtonVisible);
 
-function handleButtonClick() {
-  // Logic to generate 3 random students and give the option to look up for them
-  // This logic should mirror what you have in your popup.js
-}
