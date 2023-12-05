750a7942fe47ca12f98dfb58e56d1a72f3822d5c
Ran Zaaroor <Ran.zaaroor@gmail.com>
2023-12-05 11:21:20 +0200

    Updated the PoliTalk button with CSS style to match LinkedIn's

--- PoliTalk/contentScript.js
+++ PoliTalk/contentScript.js
@@ -1,3 +1,23 @@
+function injectCSS() {
+  const link = document.createElement('link');
+  link.href = chrome.runtime.getURL('extensionButton.css');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+}
+
+function addHoverEffect(button, icon) {
+  const normalIconSrc = chrome.runtime.getURL('images/PoliTalk.png'); // Normal icon
+  const hoverIconSrc = chrome.runtime.getURL('images/PoliTalkHover.png'); // Hover icon
+
+  button.onmouseover = function () {
+    icon.src = hoverIconSrc; // Change to hover icon
+  };
+  button.onmouseout = function () {
+    icon.src = normalIconSrc; // Revert to normal icon
+  };
+}
+
 function createLinkedInButton() {
   if (document.querySelector('.extension_button')) {
     return; // If the button exists, exit the function
@@ -6,15 +26,29 @@ function createLinkedInButton() {
   if (navItems) {
     const extensionButton = document.createElement('li');
     extensionButton.className = 'extension_button';
+
+    // Create a container for the icon and text
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'icon_container';
+
+    // Create and add the icon
     const icon = document.createElement('img');
     icon.src = chrome.runtime.getURL('images/PoliTalk.png');
+    iconContainer.appendChild(icon);
 
-    styleExtensionButton(extensionButton, icon);
-    extensionButton.appendChild(icon);
+    // Create and add the text
+    const text = document.createElement('span');
+    text.textContent = 'PoliTalk';
+    iconContainer.appendChild(text);
+
+    // Add the icon container to the button
+    extensionButton.appendChild(iconContainer);
+
+    // Style the button and icon
+    addHoverEffect(extensionButton, icon);
 
     extensionButton.addEventListener('click', handleButtonClick);
     navItems.insertBefore(extensionButton, navItems.firstChild);
-    injectCSS();
   }
 }
 
@@ -28,30 +62,9 @@ const observer = new MutationObserver(function (mutations) {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
+injectCSS();
 
 function handleButtonClick() {
   // Logic to generate 3 random students and give the option to look up for them
   // This logic should mirror what you have in your popup.js
-}
-
-function styleExtensionButton(button, icon) {
-  button.style.cursor = 'pointer';
-  button.style.padding = '0 10px';
-  button.style.display = 'flex';
-  button.style.alignItems = 'center';
-  button.style.justifyContent = 'center';
-
-  icon.style.width = '40px';
-  icon.style.height = '40px';
-
-  // Add hover effect
-  const normalIconSrc = chrome.runtime.getURL('images/PoliTalk.png'); // Normal icon
-  const hoverIconSrc = chrome.runtime.getURL('images/PoliTalkHover.png'); // Hover icon
-
-  button.onmouseover = function() {
-    icon.src = hoverIconSrc; // Change to hover icon
-  };
-  button.onmouseout = function() {
-    icon.src = normalIconSrc; // Revert to normal icon
-  };
 }
