0224e6a8a133e97f1392927ae205cafc9fd8c559
idanye <idanyehiel@gmail.com>
2023-12-13 20:14:17 +0200

    Created initial Login page

---
+++ admin-dashboard/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+const path = require('path');
+
+app.use(express.json()); // For parsing application/json
+app.use(express.static('public')); // Serve static files from 'public' directory
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Basic user list for demonstration
+const users = [{ username: "adminreich", passwordHash: "$2y$10$" }];
+
+app.post('/login', async (req, res) => {
+    const user = users.find(u => u.username === req.body.username);
+    if (!user) {
+        return res.status(400).send('Cannot find user');
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.passwordHash)) {
+            res.send('Success');
+        } else {
+            res.send('Not Allowed');
+        }
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
+
+async function hashPassword(password) {
+    const saltRounds = 10; // It's common to use 10 rounds
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Password hashing failed');
+    }
+}
